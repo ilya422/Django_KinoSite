adb501b1d3957baa62cd71e4d3e324d7747ac9cf
Ilya Barinov <bi@certit.ru>
2025-01-10 17:12:00 +0500

    Рефакторинг

--- kinopoisk/kinosite/admin.py
+++ kinopoisk/kinosite/admin.py
@@ -40,7 +40,7 @@ class FilmStaffPhotoInline(admin.TabularInline):
 
 class BaseModelsAdmin(admin.ModelAdmin):
     """
-        Админ панель базовых моделей
+    Админ панель базовых моделей
     """
     search_fields = ('name', )
     readonly_fields = ('created_at', 'updated_at')
@@ -48,7 +48,7 @@ class BaseModelsAdmin(admin.ModelAdmin):
 
 class FilmsAdmin(BaseModelsAdmin):
     """
-        Админ панель фильма
+    Админ панель фильма
     """
     list_display = ('id', 'name', 'released_at', 'created_at', 'updated_at')
     list_display_links = ('id', 'name')
@@ -58,7 +58,7 @@ class FilmsAdmin(BaseModelsAdmin):
 
 class FilmStaffAdmin(BaseModelsAdmin):
     """
-        Админ панель работников над фильмом
+    Админ панель работников над фильмом
     """
     list_display = ('id', 'full_name', 'birthday', 'created_at', 'updated_at')
     search_fields = ('full_name', )
@@ -67,6 +67,8 @@ class FilmStaffAdmin(BaseModelsAdmin):
 
 # endregion
 
+# region Register
+
 admin.site.register(Films, FilmsAdmin)
 admin.site.register(FilmStaff, FilmStaffAdmin)
 admin.site.register(FilmGenres, BaseModelsAdmin)
@@ -74,3 +76,4 @@ admin.site.register(Countries, BaseModelsAdmin)
 admin.site.register(FilmPhotoTypes, BaseModelsAdmin)
 admin.site.register(FilmStaffTypes, BaseModelsAdmin)
 
+# endregion
